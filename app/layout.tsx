d79b7c81158b20e12fa11e8bf36bafbcc06fef29
Rolando Valle <rolandovalle0428@gmail.com>
2026-05-31 19:14:53 +0800

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import "./globals.css"
 import { siteConfig } from "@/content/site"
 import { ClientLayout } from "@/components/client-layout"
 
-const siteUrl = process.env.NEXT_PUBLIC_SITE_URL ?? "https://kaezar-isaiahnuel-baptism.vercel.app/"
+const siteUrl = process.env.NEXT_PUBLIC_SITE_URL ?? "https://kaezar-isaiahnuel-Gallardo-baptism.vercel.app/"
 const canonicalUrl = siteUrl.replace(/\/$/, "")
 const desktopHero = "/Details/newLinkPreview.png"
 const mobileHero = "/Details/newLinkPreview.png"
